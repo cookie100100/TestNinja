f4748da3e93fd59c51e50c623883947a3e1c8e50
xulongyang <longyang.xu12@gmail.com>
2026-02-23 12:06:27 -0500

    022320261206 commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 
 dependencies {
     testImplementation( "junit:junit:4.13.2")
-    testImplementation( "org.seleniumhq.selenium:selenium-java:4.18.1")
+    testImplementation( "org.seleniumhq.selenium:selenium-java:4.41.0")
     testImplementation( "io.cucumber:cucumber-java:7.14.0")
     testImplementation( "io.cucumber:cucumber-junit:7.14.0")
     testImplementation( "io.github.bonigarcia:webdrivermanager:5.7.0")
